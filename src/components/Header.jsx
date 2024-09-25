d40b7daff6ed370daef156eb4b311bda9a795b20
ankush5thakur <142628787+ankush5thakur@users.noreply.github.com>
2024-09-25 22:13:57 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-slate-600 text-white p-4  text-center flex items-center justify-center">
       <img src="./../logo512.png" alt="Logo" className="h-10 w-10 mr-2" />
-      <h1 className="text-3xl font-bold">NITH Result</h1>
+      <h1 className="text-3xl font-bold">AcademiaResults</h1>
     </header>
   );
 };
